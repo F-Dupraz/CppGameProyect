1f0e624378f9cfae711d339a90d2ad76e0f5eb4e
Fabricio Dupraz <duprazfabricioangel@gmail.com>
2024-09-06 09:21:41 -0300

    Creating a game object factory so I can read the json file.

---
+++ Game-1/GameObjectFactory.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+#include "json.hpp"
+#include "GameObject.h"
+
+
+class BaseCreator
+{
+public:
+	virtual GameObject* createGameObject() const = 0;
+	virtual ~BaseCreator() {}
+};
+
+class GameObjectFactory
+{
+public:
+
+	bool registerTyoe(std::string typeID, BaseCreator* pCreator)
+	{
+		std::map<std::string, BaseCreator*>::iterator it = m_creators.find(typeID);
+
+		if (it != m_creators.end())
+		{
+			delete pCreator;
+			return false;
+		}
+
+		m_creators[typeID] = pCreator;
+		return true;
+	}
+
+	GameObject* create(std::string typeID)
+	{
+		std::map<std::string, BaseCreator*>::iterator it = m_creators.find(typeID);
+		
+		if (it == m_creators.end())
+		{
+			std::cout << "Could not find type: " << typeID << std::endl;
+			return NULL;
+		}
+
+		BaseCreator* pCreator = (*it).second;
+		return pCreator->createGameObject();
+	}
+
+private:
+	std::map<std::string, BaseCreator*> m_creators;
+};
